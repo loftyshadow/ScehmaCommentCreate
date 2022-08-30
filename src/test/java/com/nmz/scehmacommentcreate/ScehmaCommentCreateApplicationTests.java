3a37e8410ccedc6deaf4771671aa6ebd1c86f5a1
niemingzhi <niemingzhi@sinsoft.com.cn>
2022-08-30 15:44:43 +0800

    增加数据库配置，流关闭和Stringbuild优化

--- src/test/java/com/nmz/scehmacommentcreate/ScehmaCommentCreateApplicationTests.java
+++ src/test/java/com/nmz/scehmacommentcreate/ScehmaCommentCreateApplicationTests.java
@@ -5,6 +5,7 @@ import com.nmz.scehmacommentcreate.pojo.TableColumns;
 import com.nmz.scehmacommentcreate.pojo.Tables_in_hklis;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,12 +15,15 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
 class ScehmaCommentCreateApplicationTests {
+    @Value("${database}")
+    String datebase;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -28,34 +32,44 @@ class ScehmaCommentCreateApplicationTests {
     void contextLoads() {
     }
 
-    public void testeee(String tablename, Map map) throws Exception {
-        String table = tablename;
+    public void createSQL(String tablename, Map<String, String> map) throws Exception {
         BufferedWriter bw = null;
-        List<Tables_in_hklis> list1 = jdbcTemplate.query("SHOW TABLES IN hklis WHERE Tables_in_hklis = '" + table + "'", new BeanPropertyRowMapper<Tables_in_hklis>(Tables_in_hklis.class));
-
-        if (list1.size() == 0)
+        /*判断是否在数据库中存在对应schema的表*/
+        List<Tables_in_hklis> list1 = jdbcTemplate.query("SHOW TABLES IN " + datebase + " WHERE Tables_in_" + datebase +" = '" + tablename + "'", new BeanPropertyRowMapper<Tables_in_hklis>(Tables_in_hklis.class));
+        if (list1.size() == 0) {
             return;
-        List<TableColumns> list = jdbcTemplate.query("show columns from " + table, new BeanPropertyRowMapper<TableColumns>(TableColumns.class));
-        StringBuffer sb = new StringBuffer();
+        }
+        List<TableColumns> list = jdbcTemplate.query("SHOW COLUMNS FROM " + tablename, new BeanPropertyRowMapper<TableColumns>(TableColumns.class));
+        StringBuilder sb = new StringBuilder();
         for (TableColumns tableColumns : list) {
             if (map.get(tableColumns.getField()) != null) {
-
-                sb.append("ALTER TABLE " + tablename + " MODIFY ");
-                sb.append("`" + tableColumns.getField() + "` " + tableColumns.getType());
+                sb.append("ALTER TABLE ").append(tablename).append(" MODIFY ");
+                sb.append("`").append(tableColumns.getField()).append("` ").append(tableColumns.getType());
                 if ("NO".equals(tableColumns.getNull())) {
                     sb.append(" NOT NULL ");
                 }
-                sb.append(" COMMENT '" + map.get(tableColumns.getField()) + "';\n");
+                sb.append(" COMMENT '").append(map.get(tableColumns.getField())).append("';\n");
             }
         }
         bw = new BufferedWriter(new FileWriter("D:\\增加注释sql.sql", true));
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.close();
+        try {
+            bw.write(sb.toString());
+            bw.newLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Test
-    public void getmap() throws Exception {
+    public void getMap() throws Exception {
         String filePath = "D:\\schema\\";
         File file = new File(filePath);
         Filter filter = new Filter("Schema.java");
@@ -64,7 +78,7 @@ class ScehmaCommentCreateApplicationTests {
         boolean start = false;
         String tablename, finname, notes = "";
         for (String filename : files) {
-            Map map = new HashMap<>();
+            Map<String, String> map = new HashMap<>();
             tablename = filename.substring(0, filename.indexOf("Schema"));
             finname = filePath + filename;
             FileReader reader = new FileReader(finname);
@@ -86,7 +100,7 @@ class ScehmaCommentCreateApplicationTests {
                     }
                 }
             }
-            testeee(tablename, map);
+            createSQL(tablename, map);
             continue;
         }
         System.exit(0);
